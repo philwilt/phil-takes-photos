a248bbaa2cc46df4db7756aa01824b623db68022
Phil Wilt <phillip.wilt@gmail.com>
2025-10-15 03:28:57 -0700

    Deploy

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,9 +3,13 @@ import { GalleryImage } from "../gallery/GalleryImage";
 import { type Caption, GalleryCaption } from "../gallery/GalleryCaption";
 import { useEffect } from "react";
 
-const Modal = ({ src, caption, handleClose }) => {
+const Modal: React.FC<{
+  src: string;
+  caption: Caption;
+  handleClose: () => void;
+}> = ({ src, caption, handleClose }) => {
   useEffect(() => {
-    const close = (e) => {
+    const close = (e: KeyboardEvent) => {
       if (e.keyCode === 27) {
         handleClose();
       }
@@ -17,18 +21,17 @@ const Modal = ({ src, caption, handleClose }) => {
   return (
     <div className="modal-backdrop max-h-fit">
       <div className="modal">
-        <>
-          <div className="gallery__item rounded-md p-4 shadow-lg shadow-black-500/10 items-center justify-items-center">
-            <GalleryImage src={src} full />
-            <GalleryCaption caption={caption} />
-            <div
-              className="mt-4 text-lg text-center font-bold dark:text-rose-600 text-rose-600 hover:text-purple-400 dark:text-background mt-2 cursor-pointer"
-              onClick={() => handleClose()}
-            >
-              (X) Close
-            </div>
-          </div>
-        </>
+        <div
+          className="pt-2 mr-6 text-lg text-right font-bold dark:text-rose-600 text-rose-600 hover:text-purple-400 dark:text-background mt-2 cursor-pointer"
+          onClick={() => handleClose()}
+        >
+          (X) Close
+        </div>
+
+        <div className="gallery__item rounded-md p-4 shadow-lg shadow-black-500/10 items-center justify-items-center">
+          <GalleryImage src={src} full />
+          <GalleryCaption caption={caption} />
+        </div>
       </div>
     </div>
   );

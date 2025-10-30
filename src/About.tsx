b75fee808d237f4cbbba323a695383e9ab206240
Phil Wilt <phillip.wilt@gmail.com>
2025-10-29 21:49:25 -0700

    Mobile fixes

--- src/About.tsx
+++ src/About.tsx
@@ -18,6 +18,7 @@ function About() {
               height={36}
             />
           </Link>
+
           <Link
             to="/"
             className="text-3xl font-large flex flex-row content-center justify-center"
@@ -26,18 +27,21 @@ function About() {
               Phil Takes Photos
             </h1>
           </Link>
+
           <h2 className="text-xl font-large flex flex-row text-center">
             capturing the surreal and the serene
           </h2>
-          <div className="flex flex-row ">
-            <div className="shadow-md shadow-black-950/10 p-2 items-center flex flex-col">
-              <div className=" text-xl font-large flex flex-row mt-2 text-center rounded-sm pl-10 pr-40 pt-10 pb-20 bg-[url(/about-me.jpg)] bg-cover h-[260px] w-lg">
-                <div className="text-xl font-large font-bold flex flex-row mt-2 text-center bg-neutral-50 text-neutral-600 pl-5 pr-5 h-fit">
-                  Phil
+
+          <div className="flex lg:flex-row flex-col">
+            <div className="shadow-md shadow-black-950/10 p-2 flex flex-col">
+              <div className=" text-xl font-large flex flex-row mt-2 rounded-sm pl-2 sm:pl-10 pr-40 pt-2 pb-20 bg-[url(/about-me.jpg)] bg-cover bg-top-right md:bg-center h-[260px] sm:w-sm">
+                <div className="text-xl font-large font-bold flex flex-row mt-2 bg-neutral-50 text-neutral-600 pl-3 pr-5 h-fit">
+                  About Phil
                 </div>
               </div>
             </div>
-            <div className="flex sm:flex-row pt-4 pb-12 pl-12">
+
+            <div className="flex pt-4 pb-12 pl-1">
               <p>
                 My journey as a photographer began in my youth, exploring the
                 streets of Seattle with a growing passion for analog
@@ -56,8 +60,9 @@ function About() {
               </p>
             </div>
           </div>
-          <div className="flex flex-row ">
-            <div className="flex flex-row pt-4 pr-12">
+
+          <div className="flex lg:flex-row flex-col-reverse items-center">
+            <div className="flex pt-4 pr-1">
               <p>
                 Infrared photography captures light that lies beyond the visible
                 spectrum—specifically infrared wavelengths that the human eye
@@ -71,8 +76,9 @@ function About() {
                 exists all around us but remains invisible to ordinary
               </p>
             </div>
-            <div className="shadow-md shadow-black-950/10 p-2 flex flex-col">
-              <div className=" text-xl font-large flex  mt-2 rounded-sm  pl-10 mr-0 pt-10 pb-20 bg-[url(/about-ir.jpg)] bg-cover h-[260px] w-lg">
+
+            <div className="shadow-md shadow-black-950/10 p-2">
+              <div className=" text-xl font-large flex mt-2 rounded-sm  pl-10 pr-40 pt-10 pb-20 bg-[url(/about-ir.jpg)] bg-cover h-[260px] sm:w-sm">
                 <div className="text-xl font-large font-bold flex flex-row mt-2 text-left bg-neutral-50 text-neutral-600 pl-5 pr-5 h-fit">
                   Infrared
                 </div>
--- src/Home.tsx
+++ src/Home.tsx
@@ -14,14 +14,17 @@ function Home() {
             height={36}
             className="text-3xl font-large flex flex-row content-center"
           />
+
           <h1 className="text-3xl font-large flex flex-row">
             Phil Takes Photos
           </h1>
-          <h2 className="text-xl font-large flex flex-row  text-center">
+
+          <h2 className="text-xl font-large flex flex-row text-center">
             capturing the surreal and the serene
           </h2>
-          <div className="flex flex-row">
-            <Link to={"/galleries"}>
+
+          <div className="flex flex-col sm:flex-row">
+            <Link to={"/galleries"} className="flex flex-row">
               <div className="shadow-md shadow-black-950/10 p-2 transform transition duration-300 hover:scale-102">
                 <div className=" text-xl font-large flex flex-row mt-2 text-center rounded-sm pl-10 pr-40 pt-10 pb-20 bg-[url(/hero-1.png)] bg-cover">
                   <div className="text-xl font-large font-bold flex flex-row mt-2 text-center bg-neutral-50 pl-5 pr-5">
@@ -31,10 +34,10 @@ function Home() {
               </div>
             </Link>
 
-            <Link to={"/about"}>
+            <Link to={"/about"} className="flex flex-row">
               <div className="shadow-md shadow-black-950/10 p-2 transform transition duration-300 hover:scale-102">
                 <div className=" text-xl font-large flex flex-row mt-2 text-center rounded-sm pl-10 pr-40 pt-10 pb-20 bg-[url(/about-me.jpg)] bg-cover">
-                  <div className="text-xl font-large font-bold flex flex-row mt-2 text-center bg-neutral-50 pl-5 pr-5">
+                  <div className="text-xl font-large font-bold flex flex-row mt-2 text-center bg-neutral-50 pl-5 pr-5 ml-3 mr-2">
                     About
                   </div>
                 </div>
